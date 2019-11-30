a0881d17e1e3756c6ea12a11c94bd1003e814a85
Phillip Davis <phillip.davis3@tafensw.net.au>
2019-11-30 20:08:00 +1100

    Can now view price and seatlocation of tickets selected

--- Ssample/ViewModel/Buying tickets/PurchaseTicketsViewModel.cs
+++ Ssample/ViewModel/Buying tickets/PurchaseTicketsViewModel.cs
@@ -1,10 +1,8 @@
-﻿using System;
+﻿using SimpleWPF.ViewModels;
+using Ssample.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SimpleWPF.ViewModels;
-using Ssample.Model;
 
 namespace Ssample.ViewModel.Buying_tickets
 {
@@ -12,8 +10,24 @@ namespace Ssample.ViewModel.Buying_tickets
     {
         public PurchaseTicketsViewModel()
         {
+            CustomerDatabaseEntities context = new CustomerDatabaseEntities();
+            Tickets = (from data in context.Ticket_Details select data).ToList();
+
+            string seatLocations = Properties.Settings.Default.SeatLocation;
+            String[] separator = { "," };
+            String[] strList = seatLocations.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var seatLocation in strList)
+            {
+                var item = Tickets.FindAll(i => i.seatLocation == seatLocation && i.eventTitle == Properties.Settings.Default.EventTitle);
+                foreach (var ticketDetails in item) ListOfMatchingTickets.Add(ticketDetails);
+            }
             
         }
+
+        public List<Ticket_Details> ListOfMatchingTickets { get; set; } = new List<Ticket_Details>();
+
+        public List<Ticket_Details> Tickets { get; set; }
 
         private string _email;
         public string Email
--- Ssample/Views/Buying tickets/PurchaseTicketsView.xaml.cs
+++ Ssample/Views/Buying tickets/PurchaseTicketsView.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Ssample.ViewModel.Buying_tickets;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Ssample.Views.Buying_tickets
 {
@@ -23,6 +12,7 @@ namespace Ssample.Views.Buying_tickets
         public PurchaseTicketsView()
         {
             InitializeComponent();
+            DataContext = new PurchaseTicketsViewModel();
         }
 
         private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
