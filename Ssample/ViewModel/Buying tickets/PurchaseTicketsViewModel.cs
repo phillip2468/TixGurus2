9ffbaf5fb662b444f3740ba44f7bbd5d191703ac
Phillip Davis <phillip.davis3@tafensw.net.au>
2019-11-30 22:32:10 +1100

    Can now show the total price

--- Ssample/ViewModel/Buying tickets/PurchaseTicketsViewModel.cs
+++ Ssample/ViewModel/Buying tickets/PurchaseTicketsViewModel.cs
@@ -3,33 +3,97 @@ using Ssample.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
+using SimpleWPF.Input;
+using Ssample.Properties;
 
 namespace Ssample.ViewModel.Buying_tickets
 {
     public class PurchaseTicketsViewModel : NavigationViewModelBase
     {
+        /// <summary>
+        /// Command for navigating backwards
+        /// </summary>
+        public ICommand NavCommand { get; set; }
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor for purchasing tickets
+        /// </summary>
         public PurchaseTicketsViewModel()
         {
+            NavCommand = new RelayCommand<NavigationViewModelBase>(Nav);
+
             CustomerDatabaseEntities context = new CustomerDatabaseEntities();
             Tickets = (from data in context.Ticket_Details select data).ToList();
 
-            string seatLocations = Properties.Settings.Default.SeatLocation;
+            string seatLocations = Settings.Default.SeatLocation;
             String[] separator = { "," };
             String[] strList = seatLocations.Split(separator, StringSplitOptions.RemoveEmptyEntries);
 
             foreach (var seatLocation in strList)
             {
-                var item = Tickets.FindAll(i => i.seatLocation == seatLocation && i.eventTitle == Properties.Settings.Default.EventTitle);
+                var item = Tickets.FindAll(i => i.seatLocation == seatLocation && i.eventTitle == Settings.Default.EventTitle);
                 foreach (var ticketDetails in item) ListOfMatchingTickets.Add(ticketDetails);
             }
-            
+
+        }
+
+        private decimal SetTotalPrice()
+        {
+            string seatLocations = Settings.Default.SeatLocation;
+            String[] separator = { "," };
+            String[] strList = seatLocations.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            decimal total = 0;
+
+            foreach (var seatLocation in strList)
+            {
+                var item = Tickets.FindAll(i => i.seatLocation == seatLocation && i.eventTitle == Settings.Default.EventTitle);
+                foreach (var ticketDetails in item) total += ticketDetails.price;
+            }
+
+            return total;
+        }
+
+        #endregion
+
+        private void Nav(NavigationViewModelBase viewModel)
+        {
+            Navigate(viewModel);
         }
 
         public List<Ticket_Details> ListOfMatchingTickets { get; set; } = new List<Ticket_Details>();
 
         public List<Ticket_Details> Tickets { get; set; }
 
+        private Booked_Tickets_Details CurrentTicket { get; set; }
+
+        private List<Event_Details> Event;
+
+        private decimal _totalPrice;
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                if (_totalPrice == 0)
+                {
+                    _totalPrice = SetTotalPrice() * (decimal) 1.15;
+                }
+
+                return _totalPrice;
+            }
+            set => _totalPrice = value;
+        }
+
+        #region Properties
+
         private string _email;
+        /// <summary>
+        /// Property for email
+        /// </summary>
         public string Email
         {
             get { return _email; }
@@ -39,7 +103,11 @@ namespace Ssample.ViewModel.Buying_tickets
                 OnPropertyChanged($"Email");
             }
         }
+
         private string _fullName;
+        /// <summary>
+        /// Property for fullname
+        /// </summary>
         public string FullName
         {
             get { return _fullName; }
@@ -51,6 +119,9 @@ namespace Ssample.ViewModel.Buying_tickets
         }
 
         private string _address;
+        /// <summary>
+        /// Property for address
+        /// </summary>
         public string Address
         {
             get { return _address; }
@@ -61,8 +132,8 @@ namespace Ssample.ViewModel.Buying_tickets
             }
         }
 
-        private Booked_Tickets_Details CurrentTicket { get; set; }
-        private List<Event_Details> Event;
+        #endregion
+
 
         private void SaveChanges()
         {
--- Ssample/ViewModel/Buying tickets/ViewEventsViewModel.cs
+++ Ssample/ViewModel/Buying tickets/ViewEventsViewModel.cs
@@ -1,45 +1,81 @@
-﻿using System;
+﻿using SimpleWPF.Input;
+using SimpleWPF.ViewModels;
+using Ssample.Model;
+using Ssample.Properties;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
-using SimpleWPF.Input;
-using SimpleWPF.ViewModels;
-using Ssample.Model;
-using Ssample.Views.Buying_tickets;
-using Syncfusion.UI.Xaml.Grid;
 
 namespace Ssample.ViewModel.Buying_tickets
 {
+    /// <summary>
+    /// A class responsible for viewing the details about an event
+    /// </summary>
     public class ViewEventsViewModel : NavigationViewModelBase
     {
+        #region Fields
+        /// <summary>
+        /// Buying tickets view model declaration
+        /// </summary>
         private NavigationViewModelBase buyingTicketsViewModel;
 
+        #endregion
+
+        #region Commands
+        /// <summary>
+        /// Navigation command responsible for
+        /// moving back to the default view
+        /// </summary>
         public ICommand NavCommand { get; set; }
+
+        /// <summary>
+        /// Navigation command responsible for moving
+        /// to the purchasing of tickets view
+        /// </summary>
         public ICommand NavCommand2 { get; set; }
+
+        #endregion
+
+        #region Constructor
+
         public ViewEventsViewModel()
         {
+            //Initialization of the next view model
             buyingTicketsViewModel = new PurchaseTicketsViewModel();
 
+            //Setting the data context
             CustomerDatabaseEntities context = new CustomerDatabaseEntities();
 
+            //Setting the navigation command parameters
             NavCommand = new RelayCommand<NavigationViewModelBase>(Nav);
             NavCommand2 = new RelayCommand<NavigationViewModelBase>(Nav2);
-            Tickets = (from data in context.Ticket_Details where data.eventTitle == Properties.Settings.Default.EventTitle select data).ToList();
+
+            //Provide an item source for the data grid
+            Tickets = (from data in context.Ticket_Details where data.eventTitle == Settings.Default.EventTitle select data).ToList();
 
         }
-        
+
+        #endregion
+
+        #region Helper functions
+
+        /// <summary>
+        /// Navigates back to the previous page (the default view model)
+        /// </summary>
+        /// <param name="viewModel">The view model</param>
         private void Nav(NavigationViewModelBase viewModel)
         {
-            Properties.Settings.Default.SeatLocation = "";
+            Settings.Default.SeatLocation = "";
             Navigate(viewModel);
         }
 
+        /// <summary>
+        /// Navigates to the purchasing of tickets page
+        /// </summary>
+        /// <param name="viewModel">The viewmodel</param>
         private void Nav2(NavigationViewModelBase viewModel)
         {
             if (SaveSeatSelection() != true)
@@ -52,9 +88,29 @@ namespace Ssample.ViewModel.Buying_tickets
             }
         }
 
+        #endregion
+
+        #region Properties for lists
+        /// <summary>
+        /// Property for tickets
+        /// </summary>
         public List<Ticket_Details> Tickets { get; set; }
 
+        /// <summary>
+        /// Property for events
+        /// </summary>
+        public List<Event_Details> Event { get; set; }
+
+        private List<Ticket_Details> tickets = new List<Ticket_Details>();
+
+        #endregion
+
+        #region Properties
+
         private BitmapImage _image;
+        /// <summary>
+        /// Loads the image from the view model
+        /// </summary>
         public BitmapImage Image
         {
             get
@@ -93,13 +149,15 @@ namespace Ssample.ViewModel.Buying_tickets
             return image;
         }
 
-        public List<Event_Details> Event { get; set; }
-
+        /// <summary>
+        /// A function which loads the image
+        /// </summary>
+        /// <returns></returns>
         public byte[] LoadImage()
         {
             CustomerDatabaseEntities context = new CustomerDatabaseEntities();
             Event = (List<Event_Details>)(from data in context.Event_Details select data).ToList();
-            var selectedEvent = Event.Find(x => x.eventTitle == Properties.Settings.Default.EventTitle);
+            var selectedEvent = Event.Find(x => x.eventTitle == Settings.Default.EventTitle);
             byte[] selectedImage = selectedEvent.eventLayout;
             return selectedImage;
         }
@@ -122,12 +180,20 @@ namespace Ssample.ViewModel.Buying_tickets
             }
         }
 
-        private List<Ticket_Details> tickets = new List<Ticket_Details>();
+        #endregion
+
+        #region Save to database
+
+        /// <summary>
+        /// A function which returns a true or false if the typed in
+        /// seat is correct
+        /// </summary>
+        /// <returns></returns>
         private bool SaveSeatSelection()
         {
             CustomerDatabaseEntities context = new CustomerDatabaseEntities();
             Tickets = (from data in context.Ticket_Details select data).ToList();
-            var selectedEvent = tickets.Find(x => x.eventTitle == Properties.Settings.Default.EventTitle);
+            var selectedEvent = tickets.Find(x => x.eventTitle == Settings.Default.EventTitle);
 
             if (SeatPlace == null) 
             {
@@ -136,18 +202,22 @@ namespace Ssample.ViewModel.Buying_tickets
 
             else
             {
-                Properties.Settings.Default.SeatLocation = SeatPlace.Trim();
+                Settings.Default.SeatLocation = SeatPlace.Trim();
                 return true;
             }
             
         }
 
+        #endregion
+
+        #region Misc
         private bool _isSelected;
         public bool IsSelected
         {
             get { return _isSelected; }
             set { OnPropertyChanged(ref _isSelected, value); }
         }
+        #endregion
 
     }
 
