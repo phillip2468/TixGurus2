26387a686e463b012236c55f5aef0011651b5d3a
Phillip Davis <phillip.davis3@tafensw.net.au>
2019-12-01 20:33:06 +1100

    Added more functionality to the administrator

--- Ssample/ViewModel/zEmployees/Customer/ModifyGuestTicketsViewModel.cs
+++ Ssample/ViewModel/zEmployees/Customer/ModifyGuestTicketsViewModel.cs
@@ -6,20 +6,55 @@ using System.Threading.Tasks;
 using System.Windows.Input;
 using SimpleWPF.Input;
 using SimpleWPF.ViewModels;
+using Ssample.Model;
 
 namespace Ssample.ViewModel.zEmployees.Customer
 {
+    /// <summary>
+    /// A class responsible for editing guest transactions/ tickets
+    /// </summary>
     public class ModifyGuestTicketsViewModel : NavigationViewModelBase
     {
+        #region Commands
+        /// <summary>
+        /// Command for moving backwards
+        /// </summary>
         public ICommand NavCommand { get; set; }
+
+        #endregion
+
+        #region Constructor
+
         public ModifyGuestTicketsViewModel()
         {
             NavCommand = new RelayCommand<NavigationViewModelBase>(Nav);
+
+            CustomerDatabaseEntities context = new CustomerDatabaseEntities();
+
+            #region Lists with data context
+
+            GuestTransactionsDetails = (from data in context.Guest_Transaction select data).ToList();
+            GuestTicketDetails = (from data in context.Guest_Ticket_Details select data).ToList();
+
+            #endregion
         }
+
+        #endregion
+
+        #region Helper functions
 
         private void Nav(NavigationViewModelBase viewModel)
         {
             Navigate(viewModel);
         }
+
+        #endregion
+
+        #region List properties
+
+        public List<Guest_Transaction> GuestTransactionsDetails { get; set; } = new List<Guest_Transaction>();
+        public List<Guest_Ticket_Details> GuestTicketDetails { get; set; } = new List<Guest_Ticket_Details>();
+
+        #endregion
     }
 }
--- Ssample/Views/zEmployees/Customer/ModifyCustomerTicketDetailsView.xaml.cs
+++ Ssample/Views/zEmployees/Customer/ModifyCustomerTicketDetailsView.xaml.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Data.Entity;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using Ssample.Model;
+using Ssample.ViewModel.zEmployees.Customer;
+using Syncfusion.UI.Xaml.Grid;
 
 namespace Ssample.Views
 {
@@ -23,6 +15,130 @@ namespace Ssample.Views
         public ModifyCustomerTicketDetailsView()
         {
             InitializeComponent();
+            DataContext = new ModifyCustomerTicketDetailsViewModel();
+            DataGridTickets.RowValidated += dataGrid_RowValidatedForCustomerTickets;
+            DataGridTickets.RecordDeleting += dataGrid_RecordDeletingForTickets;
+            DataGridTransactions.RowValidated += dataGrid_RowValidatedForCustomerTransactions;
+            DataGridTransactions.RecordDeleting += dataGrid_RecordDeletingForCustomerTransactions;
         }
+
+        #region Logic for deleting and editing rows for customer tickets
+
+        /// <summary>
+        /// Edit rows
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void dataGrid_RowValidatedForCustomerTickets(object sender, RowValidatedEventArgs args)
+        {
+            CustomerDatabaseEntities context = new CustomerDatabaseEntities();
+            Customer_Ticket_Details newRecord = args.RowData as Customer_Ticket_Details;
+            if (sender == null)
+            {
+                return;
+            }
+            Customer_Ticket_Details order = context.Customer_Ticket_Details.First(i => i.ticketId == newRecord.ticketId);
+
+            if (newRecord != null)
+            {
+                order.ticketId = newRecord.ticketId;
+                order.email = newRecord.email;
+                order.eventAddress = newRecord.eventAddress;
+                order.fullName = newRecord.fullName;
+                order.eventName = newRecord.eventName;
+                order.placeName = newRecord.placeName;
+                order.price = newRecord.price;
+                order.seatLocation = newRecord.seatLocation;
+                order.timeEnd = newRecord.timeEnd;
+                order.timeStart = newRecord.timeStart;
+            }
+
+            context.Entry(order).State = EntityState.Modified;
+            context.SaveChanges();
+            context.Dispose();
+        }
+        /// <summary>
+        /// Delete record
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void dataGrid_RecordDeletingForTickets(object sender, RecordDeletingEventArgs args)
+        {
+            CustomerDatabaseEntities context = new CustomerDatabaseEntities();
+            var item = args.Items[0] as Customer_Ticket_Details;
+            var ticketId = item.ticketId;
+            var email = item.email;
+            var eventAddress = item.eventAddress;
+            var fullName = item.fullName;
+            var eventName = item.eventName;
+            var placeName = item.placeName;
+            var price = item.price;
+            var seatLocation = item.seatLocation;
+            var timeEnd = item.timeEnd;
+            var timeStart = item.timeStart;
+
+            context.Entry(item).State = EntityState.Deleted;
+            context.SaveChanges();
+            context.Dispose();
+        }
+
+        #endregion
+
+        #region Logic for deleting and editing rows for customer transactions
+
+        /// <summary>
+        /// Edit rows
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void dataGrid_RowValidatedForCustomerTransactions(object sender, RowValidatedEventArgs args)
+        {
+            CustomerDatabaseEntities context = new CustomerDatabaseEntities();
+            Customer_Transaction newRecord = args.RowData as Customer_Transaction;
+            if (sender == null)
+            {
+                return;
+            }
+            Customer_Transaction order = context.Customer_Transaction.First(i => i.transactionId == newRecord.transactionId);
+
+            if (newRecord != null)
+            {
+                order.transactionId = newRecord.transactionId;
+                order.email = newRecord.email;
+                order.eventAddress = newRecord.eventAddress;
+                order.fullname = newRecord.fullname;
+                order.eventName = newRecord.eventName;
+                order.totalPrice = newRecord.totalPrice;
+                order.address = newRecord.address;
+            }
+
+            context.Entry(order).State = EntityState.Modified;
+            context.SaveChanges();
+            context.Dispose();
+        }
+        /// <summary>
+        /// Delete record
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        public void dataGrid_RecordDeletingForCustomerTransactions(object sender, RecordDeletingEventArgs args)
+        {
+            CustomerDatabaseEntities context = new CustomerDatabaseEntities();
+            var item = args.Items[0] as Customer_Transaction;
+            var transactionId = item.transactionId;
+            var email = item.email;
+            var eventAddress = item.eventAddress;
+            var fullname = item.fullname;
+            var eventName = item.eventName;
+            var totalPrice = item.totalPrice;
+            var address = item.address;
+
+            context.Entry(item).State = EntityState.Deleted;
+            context.SaveChanges();
+            context.Dispose();
+        }
+
+        #endregion
+
     }
 }
