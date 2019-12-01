9b85bf11d94b91927362bcbe93e30c477b198910
Phillip Davis <phillip.davis3@tafensw.net.au>
2019-12-01 22:04:49 +1100

    Last working project

--- Ssample/ViewModel/zEmployees/zData about event/MoreDataViewModel.cs
+++ Ssample/ViewModel/zEmployees/zData about event/MoreDataViewModel.cs
@@ -3,15 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using SimpleWPF.Input;
 using SimpleWPF.ViewModels;
+using Ssample.Model;
 
 namespace Ssample.ViewModel.zEmployees.zData_about_event
 {
     public class MoreDataViewModel : NavigationViewModelBase
     {
+        public ICommand NavCommand { get; set; }
         public MoreDataViewModel()
         {
-            
+            NavCommand = new RelayCommand<NavigationViewModelBase>(Nav);
+            CustomerDatabaseEntities context = new CustomerDatabaseEntities();
+            CustomerTicket = (from data in context.Customer_Ticket_Details select data).ToList();
         }
+
+        private void Nav(NavigationViewModelBase viewModel)
+        {
+            Navigate(viewModel);
+        }
+
+        public List<Customer_Ticket_Details> CustomerTicket { get; set; }
+        public List<Guest_Ticket_Details> GuestTicket { get; set; }
     }
 }
